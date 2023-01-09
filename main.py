cb0d6724b7d549634adfc079232d131be2336b9e
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-09 01:36:41 +0000

    Created display list and implemented correct letter guess showing

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ print(f'Pssst, the solution is {chosen_word}.')
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
+display = []
+for letter in chosen_word:
+  display.append("_")
+
 guess = input("Guess a letter: ").lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+i = 0
 for letter in chosen_word:
-    if letter == guess:
-        print("Right")
+    if guess == letter:
+        display[i] = guess
+        i += 1
     else:
-        print("Wrong")
-
+       i += 1
+        
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+print(display)
